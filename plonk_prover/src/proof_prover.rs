cbdd586781f5225b54334474b526dd6a96cd39be
Cyril Carlier <cyrilcarlier@hotmail.com>
2023-06-22 22:14:27 +0200

    plonk prover and verifier for process message WIP

--- plonk_prover/src/proof_prover.rs
+++ plonk_prover/src/proof_prover.rs
@@ -60,13 +60,12 @@ pub fn proveProcessMessage(
     let (prover, _) = Compiler::compile::<MakiProcessMessageCircuit>(&pp, LABEL_TRANSCRIPT)
         .expect("failed to compile circuit");
 
-    //TODO
     let circuit: MakiProcessMessageCircuit = MakiProcessMessageCircuit {
         hashed_private_key: sponge::hash(&[bytes_to_scalar(private_key)]),
-        new_state_root: sponge::hash(&[bytes_to_scalar(new_state_root)]),
-        public_key: sponge::hash(&[bytes_to_scalar(public_key)]),
-        ecdh_public_key: sponge::hash(&[bytes_to_scalar(ecdh_public_key)]),
-        ecdh_private_key: sponge::hash(&[bytes_to_scalar(ecdh_private_key)]),
+        new_state_root: bytes_to_scalar(new_state_root),
+        public_key: bytes_to_scalar(public_key),
+        ecdh_public_key: bytes_to_scalar(ecdh_public_key),
+        ecdh_private_key: bytes_to_scalar(ecdh_private_key),
     };
 
     // Generate the proof and its public inputs
--- plonk_prover/src/proof_verifier.rs
+++ plonk_prover/src/proof_verifier.rs
@@ -1,6 +1,6 @@
 use dusk_bytes::Serializable;
 use dusk_plonk::prelude::*;
-use maki_shared::types::SerializedProof;
+use maki_shared::{types::{SerializedProof, TreeRoot, PublicKey}, functions_utils::bytes_to_scalar};
 
 use crate::circuits::*;
 
@@ -25,11 +25,12 @@ pub fn verifyVoteTally(
     verifier.verify(&proof, &public_inputs)
 }
 
-// TODO : Add Public inputs and use accordingly
 pub fn verifyProcessMessage(
     public_parameters: &[u8],
+    new_state_root: TreeRoot,
+    public_key: PublicKey,
+    ecdh_public_key: PublicKey,
     proof: &SerializedProof,
-    f: JubJubAffine,
 ) -> Result<(), Error> {
     //Read public parameters
     let pp = PublicParameters::from_slice(public_parameters)?;
@@ -41,7 +42,7 @@ pub fn verifyProcessMessage(
     let proof = Proof::from_bytes(proof)?;
 
     // Create public inputs
-    let public_inputs: Vec<BlsScalar> = vec![f.get_x(), f.get_y()];
+    let public_inputs: Vec<BlsScalar> = vec![bytes_to_scalar(new_state_root), bytes_to_scalar(public_key), bytes_to_scalar(ecdh_public_key)];
 
     verifier.verify(&proof, &public_inputs)
 }
